d4c929b0734b6b54ad699f038b3b681c381b3975
MrAdam11 <a.d.omar@outlook.com>
2024-04-25 01:24:00 +0100

    making changes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import Leftmenu from './Leftmenu';
+import MainEmailView from './MainEmailView';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Leftmenu />
+      <MainEmailView />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainEmailView.jsx
@@ -0,0 +1,12 @@
+import './MainEmailView.css';
+
+
+function MainEmailView() {
+  return (
+    <main className="email-view">
+     
+    </main>
+  );
+}
+
+export default MainEmailView;
